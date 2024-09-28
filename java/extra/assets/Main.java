14412335e1bc13cc0bbb25fb0e04e63980f5c433
Daniel Park <dpark070111@gmail.com>
2024-09-28 16:13:46 -0400

    rename

--- java/extra/assets/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main( String[] args ) {
-        Car toyota = new Car(3, 5);
-        toyota.drive();
-        System.out.println(toyota.speed);
-    }
-
-}
